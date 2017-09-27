8c7b71bf942df3d4ca7a42a58071b9e63e495496
sophiesongge <sophiesongge@gmail.com>
2017-09-27 18:18:10 +0200

    add script executor

---
+++ src/main/java/ScriptEngine/PythonScriptExecutor.java
@@ -0,0 +1,42 @@
+package ScriptEngine;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import EntryPoint.EntryPoint;
+
+/**
+ * @author Sophie Song
+ * @since 27/09/2017
+ */
+public class PythonScriptExecutor {
+
+    public void pythonScriptExecutor(String command) throws IOException{
+        EntryPoint entryPoint = EntryPoint.getInstance();
+        entryPoint.gateWayServerStart();
+        try{
+            Process process = Runtime.getRuntime().exec(command);
+            InputStream fis = process.getInputStream();
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            if(process.exitValue() == 0){
+                System.out.println("Python ScriptEngine Execution OK");
+            }else{
+                System.out.println("Python ScriptEngine Execution ERROR");
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally{
+            entryPoint.gateWayServerStop();
+            System.exit(-1);
+        }
+    }
+
+}
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,10 +1,8 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import EntryPoint.EntryPoint;
+import ScriptEngine.PythonScriptExecutor;
 import ScriptEngine.PythonScriptWriter;
 import ScriptEngine.PythonCommandCreator;
 /**
@@ -13,9 +11,9 @@ import ScriptEngine.PythonCommandCreator;
  */
 public class test {
 
-    private static EntryPoint entryPoint = EntryPoint.getInstance();
     private static PythonScriptWriter psw = new PythonScriptWriter();
     private static PythonCommandCreator pcc = new PythonCommandCreator();
+    private static PythonScriptExecutor pse = new PythonScriptExecutor();
 
     private final static String fileContent = "import numpy as np\n" +
             "a = np.arange(15).reshape(3, 5)\n" +
@@ -23,31 +21,10 @@ public class test {
             "print(variables[\"test0\"]+\" test success\")";
 
     public static void main(String[] args) throws IOException {
-        entryPoint.gateWayServerStart();
         File pythonFile = psw.writeFileToDisk(fileContent);
         String command = pcc.createPythonExecutionCommand(pythonFile, "python3");
         System.out.println(command);
 
-        try{
-            Process process = Runtime.getRuntime().exec(command);
-            InputStream fis = process.getInputStream();
-            InputStreamReader isr = new InputStreamReader(fis);
-            BufferedReader br = new BufferedReader(isr);
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-            if(process.exitValue() == 0){
-                System.out.println("Python ScriptEngine Execution OK");
-            }else{
-                System.out.println("Python ScriptEngine Execution ERROR");
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }finally{
-            entryPoint.gateWayServerStop();
-            System.exit(-1);
-        }
-
+        pse.pythonScriptExecutor(command);
     }
 }
